13708d8424bf4f79348927757a60feb4fffe6574
Stjepan Rajko <stjepan.rajko@offerpad.com>
2018-02-05 15:46:23 -0700

    Integrate two counters

--- ClientApp/components/Counter.tsx
+++ ClientApp/components/Counter.tsx
@@ -1,31 +1,24 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 
-interface CounterState {
+interface CounterProps {
     currentCount: number;
+    incrementCounter: () => void;
 }
 
-export class Counter extends React.Component<void, CounterState> {
-    constructor() {
-        super();
-        this.state = { currentCount: 0 };
-    }
+interface CounterState {
+}
 
+export class Counter extends React.Component<CounterProps, CounterState> {
     public render() {
         return <div>
             <h1>Counter</h1>
 
             <p>This is a simple example of a React component.</p>
 
-            <p>Current count: <strong>{ this.state.currentCount }</strong></p>
+            <p>Current count: <strong>{ this.props.currentCount }</strong></p>
 
-            <button onClick={ () => { this.incrementCounter() } }>Increment</button>
+            <button onClick={ this.props.incrementCounter }>Increment</button>
         </div>;
-    }
-
-    incrementCounter() {
-        this.setState({
-            currentCount: this.state.currentCount + 1
-        });
     }
 }
--- ClientApp/components/TwoCounters.tsx
+++ ClientApp/components/TwoCounters.tsx
@@ -3,13 +3,26 @@ import { RouteComponentProps } from 'react-router';
 import { Counter } from './Counter';
 
 interface TwoCountersState {
+    counters: number[]
 }
 
 export class TwoCounters extends React.Component<RouteComponentProps<{}>, TwoCountersState> {
+    constructor(props:RouteComponentProps<{}>) {
+        super(props);
+        this.state = { counters: [1, 0] }
+    }
+
     public render() {
+        const { counters } = this.state;
         return <div>
-            <Counter />
-            <Counter />
+            <Counter currentCount={counters[0]} incrementCounter={() => this.incrementCounter(0)} />
+            <Counter currentCount={counters[1]} incrementCounter={() => this.incrementCounter(1)} />
         </div>;
+    }
+
+    private incrementCounter(index: number) {
+        const { counters } = this.state;
+        counters[index] += index ? counters[0] : 1;
+        this.setState({ counters });
     }
 }
